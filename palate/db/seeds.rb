6998659d7f1b6a00abb70daa4e8ebd32bc9ed0c0
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-09 11:20:14 -0800

    add helper for rating methods in applicationcontroller and seed file created

--- palate/db/seeds.rb
+++ palate/db/seeds.rb
@@ -1,12 +1,47 @@
-User.destroy_all
-Recipe.destroy_all
-Ingredient.destroy_all
-Rating.destroy_all
+# User.destroy_all
+# Recipe.destroy_all
+# Ingredient.destroy_all
+# Rating.destroy_all
 
+50.times do
 
-User.create(first_name: "Kim", last_name: "Embry", username: "princess", email:"mistresskim@mail.com", password_digest: "boys")
-Recipe.create(name: "Beef balls", cuisine: "Spanish", course_type: "appetizer", instructions: "Don't try this at home", time: Time.now, creator_id: 1)
-Ingredient.create(item: "Beef", amount: "5", metric: "balls", recipe_id: 1)
-Rating.create(value: 5, rater_id: 1, recipe_id: 1)
+User.create(
+first_name: Faker::Name.first_name  ,
+last_name: Faker::Name.last_name,
+username: Faker::Internet.user_name,
+email: Faker::Internet.email,
+password_digest: "123"
+)
 
+end
 
+50.times do
+
+Recipe.create(
+name: Faker::Pokemon.name,
+cuisine: "#{Faker::Demographic.demonym} cousine",
+course_type: "appetizer",
+instructions: "Don't try this at home",
+time: Time.now,
+creator_id: rand(0...50))
+
+end
+
+100.times do
+
+Rating.create(
+value: rand(1...5),
+rater_id: rand(0...50),
+recipe_id: rand(0...50))
+
+end
+
+50.times do
+
+Ingredient.create(
+item: Faker::Food.ingredient,
+amount: rand(1..10),
+metric: Faker::Food.measurement,
+recipe_id: 1)
+
+end
