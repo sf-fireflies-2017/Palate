da8afa180e9c63183245fc90caab7e9d2f902085
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-09 14:21:39 -0800

    delete the changes made while trying flash[:notice]

--- palate/app/controllers/sessions_controller.rb
+++ palate/app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-			flash.keep[:notice] = "Successfully logged in."
       redirect_to user_path(@user)
     else
       render '/login'
