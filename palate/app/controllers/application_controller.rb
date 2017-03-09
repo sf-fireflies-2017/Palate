d00e76ffffaf6fec3bdc77a7c4ac9cfd54dac06b
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-09 12:19:50 -0800

    move average rating helper method to Recipe class as instance method and add  flash[:notice] in user log in

--- palate/app/controllers/application_controller.rb
+++ palate/app/controllers/application_controller.rb
@@ -1,19 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def average_rating (ratings_array)
-     ratings_array.reduce(0) {|sum, rating| sum+ rating.value} / ratings_array.length
-  end
+
 
   def sort_by_rating(recipes_array)
     unsorted_average_rating = {}
-    recipes_array.each do |recipe|
-      temp_votable[votable.id] = average_rating(recipe.ratings)
+    recipes_array.map do |recipe|
+       unsorted_average_rating[recipe.id] = recipe.average_rating
     end
     sorted_recipe_id = unsorted_average_rating.sort_by{|k,v| v}.reverse
     return sorted_recipe_id
 
-    #return the array of recipe ids. need to find the recipe with these id  
+    #return the array of recipe ids. need to find the recipe with these id
   end
 
 end
--- palate/app/models/recipe.rb
+++ palate/app/models/recipe.rb
@@ -1,5 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :ratings 
-  has_many :ingredients 
+  has_many :ratings
+  has_many :ingredients
+
+  def average_rating
+    ratings_array = this.ratings
+     ratings_array.reduce(0) {|sum, rating| sum+ rating.value} / ratings_array.length
+  end
 end
