d5b68666c8d4021191946e4c48f079e569bb63bf
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-09 11:34:49 -0800

    adds sessions for user auth and removes test recipe controller files

--- palate/config/routes.rb
+++ palate/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+
+  resources :recipes
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
