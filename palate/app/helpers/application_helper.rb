690d3e4a3f689d64bf8f2012d769ec3504b71865
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-09 12:11:18 -0800

    moves average rating helper to model as instance method and adds calulationto view

--- palate/app/helpers/application_helper.rb
+++ palate/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def sort_by_rating(recipes_array)
+    unsorted_average_rating = {}
+    recipes_array.each do |recipe|
+      temp_votable[votable.id] = average_rating(recipe.ratings)
+    end
+    sorted_recipe_id = unsorted_average_rating.sort_by{|k,v| v}.reverse
+    return sorted_recipe_id
+
+    #return the array of recipe ids. need to find the recipe with these id  
+  end
 end
--- palate/app/models/recipe.rb
+++ palate/app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :ratings 
   has_many :ingredients 
+
+  def average_rating
+     self.ratings.reduce(0) {|sum, rating| sum+ rating.value} / self.ratings.length
+  end
 end
