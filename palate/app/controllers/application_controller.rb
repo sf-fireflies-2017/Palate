0ae8814aa535b2ed0799cf2bc805bc4bb8b21351
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-10 12:42:31 -0800

    view top four menu

--- palate/app/controllers/application_controller.rb
+++ palate/app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
+  def index
+    @top_four = [Recipe.sort_by_rating_type("Appetizer")[0...4] ,Recipe.sort_by_rating_type("Salad")[0...4] ,Recipe.sort_by_rating_type("Entree")[0...4] ,Recipe.sort_by_rating_type("Dessert")[0...4] ]
+
+  end
 
 end
--- palate/config/routes.rb
+++ palate/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "recipes#index"
+  root "application#index"
   resources :recipes
   resources :ingredients
   resources :users
