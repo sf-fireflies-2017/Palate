63c630907bb2345031d087f30f4cedb052237d59
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-09 16:23:37 -0800

    refactor average function

--- palate/app/helpers/application_helper.rb
+++ palate/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-<<<<<<< HEAD
+
   def sort_by_rating(recipes_array)
     unsorted_average_rating = {}
     recipes_array.each do |recipe|
@@ -9,12 +9,7 @@ module ApplicationHelper
     sorted_recipe_id = unsorted_average_rating.sort_by{|k,v| v}.reverse
     return sorted_recipe_id
 
-    #return the array of recipe ids. need to find the recipe with these id  
+    #return the array of recipe ids. need to find the recipe with these id
   end
-=======
 
-
-
-
->>>>>>> d544de2bb27c5fa47e667d3b7519d7c6fa8579ea
 end
