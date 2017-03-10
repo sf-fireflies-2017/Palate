86ce074cc4539f084538acb9a7910c1c1b39e1eb
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-10 13:27:20 -0800

    attempts rating form

--- palate/app/controllers/ratings_controller.rb
+++ palate/app/controllers/ratings_controller.rb
@@ -1,18 +1,20 @@
 class RatingsController < ActionController::Base
 	def new
 		@rating = Rating.new
+		@recipe = Recipe.find(params[:id])
 	end
 
 	def create
 		@rating = Rating.new(rating_params)
-		@rating.recipe = Recipe.find(params[:id])
+		@recipe = Recipe.find(params[:id])
+		@recipe.ratings << @rating
 		@rating.rater = current_user
 	end
 
 	private
 
 	def rating_params
-		params.require(:rating).permit(:value)
+		params.require(:rating).permit(:value, :rater_id, :recipe_id)
 	end
 
 end
