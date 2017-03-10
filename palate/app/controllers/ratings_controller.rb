39d01a1e235d62462bd6e3c59875b223991e5d6b
Jack Howerton <jackhowerton@gmail.com>
2017-03-10 15:45:26 -0800

    recipe show allows users to rate, provided they have not already.. otherwise, they may delete

--- palate/app/controllers/ratings_controller.rb
+++ palate/app/controllers/ratings_controller.rb
@@ -1,14 +1,30 @@
-class RatingsController < ActionController::Base
+
+class RatingsController < ApplicationController
 	def new
 		@rating = Rating.new
 		@recipe = Recipe.find(params[:id])
 	end
 
 	def create
+		@recipe = Recipe.find(params[:recipe_id])
 		@rating = Rating.new(rating_params)
-		@recipe = Recipe.find(params[:id])
 		@recipe.ratings << @rating
 		@rating.rater = current_user
+		if @rating.save
+			redirect_to @recipe
+		else
+			render 'recipes/show'
+		end
+		
+	end
+
+	def destroy
+
+		
+	    @rating = Rating.find(params[:id])
+	    @recipe = Recipe.find(@rating.recipe)
+	    @rating.destroy
+	    redirect_to @recipe
 	end
 
 	private
