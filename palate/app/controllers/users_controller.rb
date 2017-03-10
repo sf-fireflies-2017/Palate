aee997de2168658077cee9fb44bce1f8eb11727d
Kimberly Embry <kimberly_embry@yahoo.com>
2017-03-09 16:29:35 -0800

    Add temporary navigation bar to application layout

--- palate/app/controllers/users_controller.rb
+++ palate/app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
 	def new
-		@user = User.new 
-	end 
+		@user = User.new
+	end
 
 	def create
 		@user = User.new(user_params)
@@ -11,16 +11,16 @@ class UsersController < ApplicationController
 		else
 			render :new
 		end
-	end 
+	end
 
 	def show
 		@user = User.find(params[:id])
-	end 
+	end
 
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :username, :email, :password
+		params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
   end
 
 end
